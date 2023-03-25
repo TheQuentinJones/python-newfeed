ba7767e72778602b064d596e37b16bd2913cd47c
NEO <qj82189@gmail.com>
2023-03-25 19:22:12 -0400

    added dashboard routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from app.routes import home
+from app.routes import home, dashboard
 
 def create_app(test_config=None):
   # set up app config
@@ -15,5 +15,6 @@ def create_app(test_config=None):
 
   # register routes
   app.register_blueprint(home)
-
+  app.register_blueprint(dashboard)
+  
   return app
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,1 +1,2 @@
 from .home import bp as home
+from .dashboard import bp as dashboard
