387ac44200cb575b51618f7f4c838210cd29f6e9
NEO <qj82189@gmail.com>
2023-03-26 18:15:44 -0400

    added delete and update routes

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,11 +1,35 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
+from app.models import Post
+from app.db import get_db
 
 bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 
 @bp.route('/')
 def dash():
-  return render_template('dashboard.html')
+
+  db = get_db()
+  posts = (
+  db.query(Post)
+  .filter(Post.user_id == session.get('user_id'))
+  .order_by(Post.created_at.desc())
+  .all()
+  )
+
+  return render_template(
+  'dashboard.html',
+  posts=posts,
+  loggedIn=session.get('loggedIn')
+  )
 
 @bp.route('/edit/<id>')
 def edit(id):
-  return render_template('edit-post.html')
+  # get single post by id
+  db = get_db()
+  post = db.query(Post).filter(Post.id == id).one()
+
+  # render edit page
+  return render_template(
+    'edit-post.html',
+    post=post,
+    loggedIn=session.get('loggedIn')
+  )
