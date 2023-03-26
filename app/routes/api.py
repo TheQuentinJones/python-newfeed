166dd8df954851c56fc3b349267e8b393d87d652
NEO <qj82189@gmail.com>
2023-03-26 15:29:36 -0400

    added login route

--- app/routes/api.py
+++ app/routes/api.py
@@ -38,3 +38,24 @@ def logout():
   # remove session variables
   session.clear()
   return '', 204
+
+@bp.route('/users/login', methods=['POST'])
+def login():
+  data = request.get_json()
+  db = get_db()
+
+  try:
+    user = db.query(User).filter(User.email == data['email']).one()
+  except:
+    print(sys.exc_info()[0])
+
+    return jsonify(message = 'Incorrect credentials'), 400
+
+  if user.verify_password(data['password']) == False:
+    return jsonify(message = 'Incorrect credentials'), 400
+
+  session.clear()
+  session['user_id'] = user.id
+  session['loggedIn'] = True
+
+  return jsonify(id = user.id)
