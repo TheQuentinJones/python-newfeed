fcf5fc7e3152b1cfe8d2da36e37591730b0f0d57
NEO <qj82189@gmail.com>
2023-03-26 15:11:44 -0400

    added logout route

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, session
 from app.models import User
 from app.db import get_db
+import sys
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -9,15 +10,31 @@ def signup():
   data = request.get_json()
   db = get_db()
 
-  # create a new user
-  newUser = User(
-    username = data['username'],
-    email = data['email'],
-    password = data['password']
-  )
+  try:
+    # attempt creating a new user
+    newUser = User(
+      username = data['username'],
+      email = data['email'],
+      password = data['password']
+    )
 
-  # save in database
-  db.add(newUser)
-  db.commit()
+    db.add(newUser)
+    db.commit()
+  except:
+    # insert failed, so send error to front end
+    print(sys.exc_info()[0])
+    # insert failed, so rollback and send error to front end
+    db.rollback()
+    return jsonify(message = 'Signup failed'), 500
+
+  session.clear()
+  session['user_id'] = newUser.id
+  session['loggedIn'] = True
 
   return jsonify(id = newUser.id)
+
+@bp.route('/users/logout', methods=['POST'])
+def logout():
+  # remove session variables
+  session.clear()
+  return '', 204
