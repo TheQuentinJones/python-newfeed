13994a271c640d2d01a1c05e3ea7a3b107a99a54
NEO <qj82189@gmail.com>
2023-03-26 11:20:19 -0400

    finished adding models

--- app/models/User.py
+++ app/models/User.py
@@ -1,5 +1,9 @@
 from app.db import Base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import validates
+import bcrypt
+
+salt = bcrypt.gensalt()
 
 class User(Base):
   __tablename__ = 'users'
@@ -7,3 +11,17 @@ class User(Base):
   username = Column(String(50), nullable=False)
   email = Column(String(50), nullable=False, unique=True)
   password = Column(String(100), nullable=False)
+
+  @validates('email')
+  def validate_email(self, key, email):
+    # make sure email address contains @ character
+    assert '@' in email
+
+    return email
+  
+  @validates('password')
+  def validate_password(self, key, password):
+    assert len(password) > 4
+
+    # encrypt password
+    return bcrypt.hashpw(password.encode('utf-8'), salt)
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,4 @@
 from .User import User
+from .Post import Post
+from .Comment import Comment
+from .Vote import Vote
